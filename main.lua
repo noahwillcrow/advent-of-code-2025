7f9fc10cdbf2f4816c1f89350c0ef062c9897dec
Noah Crowley <17803348+noahwillcrow@users.noreply.github.com>
2025-12-04 02:01:10 +0000

    main.lua

---
+++ main.lua
@@ -0,0 +1,31 @@
+local day = arg[1]
+local shouldUseExample = arg[2] == "example"
+
+local dayDir = string.format("./days/%s", day)
+local inputPath = dayDir .. "/input.txt"
+local solutionPath = dayDir .. "/solution.lua"
+
+if shouldUseExample then
+    inputPath = dayDir .. "/example.txt"
+end
+
+local inputContent = ""
+local inputFile = io.open(inputPath, "r")
+
+if inputFile then
+    inputContent = inputFile:read("*all")
+    inputFile:close()
+else
+    print(string.format("Error: Could not open input file at %s", inputPath))
+    os.exit(1)
+end
+
+local solutionFunc, err = loadfile(solutionPath)()
+
+if solutionFunc then
+    local result = solutionFunc(inputContent)
+    print(result)
+else
+    print(string.format("Error loading solution file at %s: %s", solutionPath, err))
+    os.exit(1)
+end
