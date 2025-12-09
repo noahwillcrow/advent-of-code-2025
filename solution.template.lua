2ec5ac7b8eea60c5640d954ea8c7897670a7c2a6
Noah Crowley <17803348+noahwillcrow@users.noreply.github.com>
2025-12-09 06:35:16 +0000

    solution template

---
+++ solution.template.lua
@@ -0,0 +1,18 @@
+local print = require("common/print-if-enabled")
+
+function parseInput(inputStr)
+    return inputStr
+end
+
+function part1(input)
+    return ""
+end
+
+function part2(input)
+    return ""
+end
+
+return function(inputStr)
+    local input = parseInput(inputStr)
+    return part1(input) .. " " .. part2(input)
+end
